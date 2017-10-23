ab6dd39c3446873be301f6368204debd9eec1d24
cortex <cortex@outlook.fr>
2017-10-23 13:31:02 +0200

    up

---
+++ TestVSTUException/Assets/Scripts/TestVSTUException.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestVSTUException : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		List<string> listNotInstanciated = null;
+		listNotInstanciated.Add("Kaboum null reference exception !");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
